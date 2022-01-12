e8780e367ac52d81b422730ccdc628d8d7a7810d
THeHooD <imth3ho0d@gmail.com>
2022-01-12 10:14:44 +0000

    adding header component and links to home, shop page and contact

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,7 +4,6 @@ import Directory from '../../components/directroy/directory.component';
 import { Link } from 'react-router-dom';
 
 const HomePage = (props) => (
-    console.log(props),
     <div className="homepage">
         <Directory />
     </div>
